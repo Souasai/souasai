18ca6aa239046eaee762c3eebb86ca10aa93f89a
真弥 岡田 <1206338@st.anan-nct.ac.jp>
2023-10-19 13:56:21 +0900

    定期実行した

--- create.js
+++ create.js
@@ -4,7 +4,7 @@ console.log("f");
 const parentId = 'gacha'; // このIDを実際のIDに変更
 
 // 生成する子divの数
-const numberOfChildDivs = 3; // この数を必要な数に変更
+const numberOfChildDivs = 0; // この数を必要な数に変更
 
 // 親divを取得
 const parentDiv = document.getElementById(parentId);
@@ -25,3 +25,19 @@ if (parentDiv) {
 } else {
   console.log(`指定したID (${parentId}) の親divが見つかりませんでした。`);
 }
+
+
+function addChoco(){
+  const childDiv = document.createElement('img');
+    
+  // 子divのスタイルを設定（サンプルのスタイル）
+  childDiv.src='https://www.tirol-choco.com/resource/images/index/img_tirol_02.png';
+  childDiv.style.width = '50px';
+  childDiv.style.height = '50px';
+  
+  // 子divを親divに追加
+  parentDiv.appendChild(childDiv);
+}
+
+
+var intervalID = setInterval(addChoco,1000);
