d650bb10fe1e3a361366a732efa061c92d96dcdb
ShoeiAzekawa <1206079@st.anan-nct.ac.jp>
2023-10-20 12:32:47 +0900

    早期退社

--- create.js
+++ create.js
@@ -2,12 +2,8 @@
 const url = new URL(location.href);
 const params = new URLSearchParams(url.search);
 
-
-
 // 親divのID
 const parentId = 'gacha'; // このIDを実際のIDに変更
-
-
 
 // 生成する子divの数
 const numberOfChildDivs = params.get('id'); // クエリパラメータのidから取得
@@ -30,63 +26,71 @@ function addChoco(){
   // 子divのスタイルを設定（サンプルのスタイル）
   var random = Math.floor( Math.random() * 9 );
   childDiv.src="https://www.tirol-choco.com/lineup/images/2303_asrt_varapackcp_" + random + ".png";
-  childDiv.style.width = '50px';
-  childDiv.style.height = '50px';
   
   // 子divを親divに追加
   parentDiv.appendChild(childDiv);
 
   console.log(++counter);
 
-  if(counter >= numberOfChildDivs){
-	
-    element = document.createElement('span');
-	element.classList.add('count-up');
-	element.textContent = numberOfChildDivs;
-	element.id = "num";
-	document.getElementById("result_num").appendChild(element);
-	console.log("値を変更しました");
-  }
+  // if(counter >= numberOfChildDivs){
+  //   element = document.createElement('span');
+  //   element.classList.add('count-up');
+  //   element.textContent = numberOfChildDivs;
+  //   element.id = "num";
+  //   document.getElementById("result_num").appendChild(element);
+  //   console.log("値を変更しました");
+  // }
 }
 
 
-intervalID = setInterval(addChoco,300);
+intervalID = setInterval(addChoco,1000);
+
+var setT1 = setTimeout(function() {  
+  Swal.fire({
+    title: '3等賞：10個',
+    text: '遊んでくれてありがとう！',
+    imageUrl: 'https://m.media-amazon.com/images/I/71j+73XfbWL._AC_UF894,1000_QL80_.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+}, 1000 * (params.get('id')) + 1000)
 
 
-//最後の数字を出す時に数字をかっこよく表示させるやつ
+// //最後の数字を出す時に数字をかっこよく表示させるやつ
 
-// カウントアップ関数
-function countUp() {
-    $("#result_num .count-up").each(function () {
-        if (!$(this).hasClass('counted')) {
-            $(this).addClass('counted');
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 1200,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                },
-            });
-        }
-    });
-}
+// // カウントアップ関数
+// function countUp() {
+//     $("#result_num .count-up").each(function () {
+//         if (!$(this).hasClass('counted')) {
+//             $(this).addClass('counted');
+//             $(this).prop('Counter', 0).animate({
+//                 Counter: $(this).text()
+//             }, {
+//                 duration: 1200,
+//                 easing: 'swing',
+//                 step: function (now) {
+//                     $(this).text(Math.ceil(now));
+//                 },
+//             });
+//         }
+//     });
+// }
 
-// Mutation Observerのコールバック関数
-const callback = function (mutationsList, observer) {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            observer.disconnect(); // 監視を一時的に解除
-            countUp();
-            observer.observe(document.querySelector('#result_num'), config); // 監視を再開
-        }
-    }
-};
+// // Mutation Observerのコールバック関数
+// const callback = function (mutationsList, observer) {
+//     for (const mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             observer.disconnect(); // 監視を一時的に解除
+//             countUp();
+//             observer.observe(document.querySelector('#result_num'), config); // 監視を再開
+//         }
+//     }
+// };
 
-// Mutation Observerのオプション
-const config = { childList: true, subtree: true };
+// // Mutation Observerのオプション
+// const config = { childList: true, subtree: true };
 
-// Mutation Observerを作成し、監視を開始
-const observer = new MutationObserver(callback);
-observer.observe(document.querySelector('#result_num'), config);
+// // Mutation Observerを作成し、監視を開始
+// const observer = new MutationObserver(callback);
+// observer.observe(document.querySelector('#result_num'), config);
