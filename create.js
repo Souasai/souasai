c07efe80722d2b73deaf1e178e8229cfd5a05c7b
真弥 岡田 <1206338@st.anan-nct.ac.jp>
2023-10-23 10:52:03 +0900

    全部きれいにした

--- create.js
+++ create.js
@@ -1,8 +1,7 @@
 data = {
-  "1": "Dランク",
-  "5": "Cランク",
-  "10": "Bランク",
-  "20": "Aランク",
+  "1": "Cランク",
+  "5": "Bランク",
+  "10": "Aランク",
   "30": "Sランク"
 }
 
@@ -13,8 +12,8 @@ const params = new URLSearchParams(url.search);
 // 親divのID
 const parentId = 'gacha'; // このIDを実際のIDに変更
 
-// 生成する子divの数
-const numberOfChildDivs = params.get('id'); // クエリパラメータのidから取得
+//chocoの数
+const numChoco = params.get('num');
 
 // 親divを取得
 const parentDiv = document.getElementById(parentId);
@@ -24,7 +23,7 @@ var intervalID;
 
 function addChoco(){
 
-  if(counter >= numberOfChildDivs - 1){
+  if(counter >= numChoco - 1){
     clearInterval(intervalID);
     console.log("繰り返しが終了しました");
   }
@@ -39,15 +38,6 @@ function addChoco(){
   parentDiv.appendChild(childDiv);
 
   console.log(++counter);
-
-  // if(counter >= numberOfChildDivs){
-  //   element = document.createElement('span');
-  //   element.classList.add('count-up');
-  //   element.textContent = numberOfChildDivs;
-  //   element.id = "num";
-  //   document.getElementById("result_num").appendChild(element);
-  //   console.log("値を変更しました");
-  // }
 }
 
 
@@ -55,50 +45,11 @@ intervalID = setInterval(addChoco,1000);
 
 var setT1 = setTimeout(function() {  
   Swal.fire({
-    title: data[params.get('id')] + '：' + params.get('id') + '個',
+    title: data[numChoco] + '：' + numChoco + '個',
     text: '遊んでくれてありがとう！',
     imageUrl: './images/result.png',
     imageWidth: 400,
     imageHeight: 200,
     imageAlt: 'Custom image',
   })
-}, 1000 * (params.get('id')) + 1000)
-
-
-// //最後の数字を出す時に数字をかっこよく表示させるやつ
-
-// // カウントアップ関数
-// function countUp() {
-//     $("#result_num .count-up").each(function () {
-//         if (!$(this).hasClass('counted')) {
-//             $(this).addClass('counted');
-//             $(this).prop('Counter', 0).animate({
-//                 Counter: $(this).text()
-//             }, {
-//                 duration: 1200,
-//                 easing: 'swing',
-//                 step: function (now) {
-//                     $(this).text(Math.ceil(now));
-//                 },
-//             });
-//         }
-//     });
-// }
-
-// // Mutation Observerのコールバック関数
-// const callback = function (mutationsList, observer) {
-//     for (const mutation of mutationsList) {
-//         if (mutation.type === 'childList') {
-//             observer.disconnect(); // 監視を一時的に解除
-//             countUp();
-//             observer.observe(document.querySelector('#result_num'), config); // 監視を再開
-//         }
-//     }
-// };
-
-// // Mutation Observerのオプション
-// const config = { childList: true, subtree: true };
-
-// // Mutation Observerを作成し、監視を開始
-// const observer = new MutationObserver(callback);
-// observer.observe(document.querySelector('#result_num'), config);
+}, 1000 * (numChoco) + 1000)
